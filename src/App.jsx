44096e8150deb01d4daf2718c1ef64aa2d262041
aliya neda <nedaaliya4@gmail.com>
2025-03-14 23:17:51 +0530

    add password generate function

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,27 @@ function App() {
   const [length,setLength] = useState(8) 
   const[numberAllowed,setnumberAllowed] =useState(false)
   const[charAllowed,setcharAllowed]=useState(false)
+  const[Password,setPassword]=useState("")
+
+  const passwordGenerator = useCallback(()=>{
+
+    const pass=""
+    const str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    
+    if (numberAllowed) str +="1234567890"
+    if(charAllowed) str += "(  @#$%^&*(){}:;'?/[]) "
+
+    for(let i = 1;i<=length;i++){
+      let index = Math.floor(Math.random()*str.length()+1)
+      pass += str.charAt(index)
+    }
+
+   setPassword(pass)
+
+
+  },[length,numberAllowed,charAllowed,setPassword])
+  
+
   return (
     <>
       <h1 className="text-4xl text-enter text-white">Password generator</h1>
